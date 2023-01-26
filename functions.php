2cb9ddcac1459fac9e28c569a2643e6cc9447959
Mike Trumbull <email@email.com>
2023-01-25 23:18:34 -0400

    First Commit

---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+
+if ( ! function_exists( 'portfolio_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various
+	 * WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme
+	 * hook, which runs before the init hook. The init hook is too late
+	 * for some features, such as indicating support post thumbnails.
+	 */
+	function portfolio_setup() {
+
+    /**
+	 * Make theme available for translation.
+	 * Translations can be placed in the /languages/ directory.
+	 */
+		load_theme_textdomain( 'portfolio', get_template_directory() . '/languages' );
+
+		/**
+		 * Add default posts and comments RSS feed links to <head>.
+		 */
+		add_theme_support( 'automatic-feed-links' );
+
+		/**
+		 * Enable support for post thumbnails and featured images.
+		 */
+		add_theme_support( 'post-thumbnails' );
+
+		/**
+		 * Add support for one custom navigation menus.
+		 */
+		register_nav_menus( array(
+			'primary'   => __( 'Primary Menu', 'portfolio' )
+		) );
+
+		/**
+		 * Enable support for the following post formats:
+		 * aside, gallery, quote, image, and video
+		 */
+		add_theme_support( 'post-formats', array( 'aside', 'gallery', 'quote', 'image', 'video' ) );
+	}
+endif; // myfirsttheme_setup
+add_action( 'after_setup_theme', 'portfolio_setup' );
+
+?>
